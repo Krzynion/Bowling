b48eac2c895f19f71e982558f824d5f8ab4f1bd6
Krzysztof Nowakowski <krzynion@o2.pl>
2022-06-29 20:59:19 +0200

    First commit

---
+++ Bowling/Form1.cs
@@ -0,0 +1,15 @@
+namespace Bowling
+{
+    public partial class frmBowling : Form
+    {
+        public frmBowling()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Frame frame = new Frame(0, 10);
+        }
+    }
+}
---
+++ Bowling/Frame.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bowling
+{
+    internal class Frame
+    {
+        private int firstRoll;
+        private int secoundRoll;
+        private Boolean strike;
+        private Boolean spare;
+        public Frame(int FirstRoll, int SecoundRoll)
+        {
+            firstRoll = FirstRoll;
+            if (FirstRoll == 10)
+                strike = true;
+            else
+            {
+                secoundRoll = SecoundRoll;
+                if(FirstRoll+SecoundRoll == 10)
+                    spare = true;
+            }
+        }
+
+        public int GetFirstRoll
+        {
+            get { return firstRoll; }
+        }
+        public int GetSecoundRoll
+        {
+            get { return secoundRoll; }
+        }
+        public Boolean IsStrike
+        {
+            get { return strike; }
+        }
+        public Boolean IsSpare
+        {
+            get { return spare; }
+        }
+    }
+}
